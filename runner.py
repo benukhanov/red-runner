dc1f2169a6b5abd37754bb545d813df1d85852ea
Ben Ukhanov <ben1zuk321@gmail.com>
2021-04-23 23:03:47 +0300

    feat: add count option

--- runner.py
+++ runner.py
@@ -3,9 +3,11 @@ import subprocess
 
 
 @click.command()
+@click.option('-c', default=1, metavar='COUNT', help='Number of times to run the given command.')
 @click.argument('command', nargs=-1, type=click.UNPROCESSED)
-def run(command):
-    subprocess.run(command)
+def run(c, command):
+    for _ in range(c):
+        subprocess.run(command)
 
 
 if __name__ == '__main__':
