f0d8ef48586533b9f97eb18ee351e7c48d707cb9
Ben Ukhanov <ben1zuk321@gmail.com>
2021-04-23 22:37:57 +0300

    feat: add run cmd

--- runner.py
+++ runner.py
@@ -1,1 +1,12 @@
-print("Hello Red Runner!")
+import click
+import subprocess
+
+
+@click.command()
+@click.argument('command', nargs=-1, type=click.UNPROCESSED)
+def run(command):
+    subprocess.run(command)
+
+
+if __name__ == '__main__':
+    run()
