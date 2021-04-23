50f2818757060c79b1c3330cc1b823bdce94498e
Ben Ukhanov <ben1zuk321@gmail.com>
2021-04-23 22:38:07 +0300

    test: add test run cmd

--- test_runner.py
+++ test_runner.py
@@ -1,2 +1,8 @@
-def test_runner():
-    pass
+from click.testing import CliRunner
+from runner import run
+
+
+def test_run_and_return_success():
+    runner = CliRunner()
+    result = runner.invoke(run, ['hostname'])
+    assert result.exit_code == 0
