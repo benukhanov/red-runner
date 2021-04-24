95d61f78c875f315d6e7ae45c8c23c41e1aaa3dd
Ben Ukhanov <ben1zuk321@gmail.com>
2021-04-24 21:55:01 +0300

    refactor: rename command variables

--- runner.py
+++ runner.py
@@ -3,23 +3,23 @@ import subprocess
 
 
 class CommandWrapper(object):
-    def __init__(self, __failed_count):
-        self.__failed_count = __failed_count
+    def __init__(self, __tries):
+        self.__tries = __tries
         self.__return_codes = {}
 
     def execute(self, command):
         process = subprocess.run(command)
         code = process.returncode
 
-        self.update_failed_count(code)
+        self.update_tries(code)
         self.update_return_codes(code)
 
-    def update_failed_count(self, code):
+    def update_tries(self, code):
         if code != 0:
-            self.__failed_count -= 1
+            self.__tries -= 1
 
-    def get_failed_count(self):
-        self.__failed_count
+    def get_tries(self):
+        self.__tries
 
     def update_return_codes(self, code):
         if len(self.__return_codes) > 0:
@@ -42,18 +42,18 @@ command_wrapper = None
 
 
 @ click.command()
-@ click.option('-c', default=1, metavar='COUNT', help='Number of times to run the given command.')
+@ click.option('-c', '--count', default=1, metavar='COUNT', help='Number of times to run the given command.')
 @ click.option('--failed-count', default=-1, metavar='N', help='Number of allowed failed command invocation attempts before giving up.')
 @ click.argument('command', default='')
-def run(c, failed_count, command):
+def run(count, failed_count, command):
     global command_wrapper
     command_wrapper = CommandWrapper(failed_count)
 
     if command != '':
-        for _ in range(c):
+        for _ in range(count):
             command_wrapper.execute(command.split())
 
-            if command_wrapper.get_failed_count() == 0:
+            if command_wrapper.get_tries() == 0:
                 break
 
     click.echo(command_wrapper.print_summary())
