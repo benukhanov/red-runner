c1099394b0e45ebfeaa895c01cf62d61fbbd320a
Ben Ukhanov <ben1zuk321@gmail.com>
2021-04-24 15:59:32 +0300

    feat: add failed count option

--- runner.py
+++ runner.py
@@ -2,13 +2,41 @@ import click
 import subprocess
 
 
-@click.command()
-@click.option('-c', default=1, metavar='COUNT', help='Number of times to run the given command.')
-@click.argument('command', nargs=-1, type=click.UNPROCESSED)
-def run(c, command):
-    for _ in range(c):
-        subprocess.run(command)
+class CommandWrapper(object):
+    def __init__(self, __failed_count):
+        self.__failed_count = __failed_count
 
+    def execute(self, command):
+        process = subprocess.run(command)
+        code = process.returncode
+
+        self.update_failed_count(code)
+
+    def update_failed_count(self, code):
+        if code != 0:
+            self.__failed_count -= 1
+
+    def get_failed_count(self):
+        self.__failed_count
+
+
+@ click.command()
+@ click.option('-c', default=1, metavar='COUNT', help='Number of times to run the given command.')
+@ click.option('--failed-count', default=-1, metavar='N', help='Number of allowed failed command invocation attempts before giving up.')
+@ click.argument('command', default='')
+def run(c, failed_count, command):
+    global command_wrapper
+    command_wrapper = CommandWrapper(failed_count)
+
+    if command != '':
+        for _ in range(c):
+            command_wrapper.execute(command.split())
+
+            if command_wrapper.get_failed_count() == 0:
+                break
+
+
+command_wrapper = None
 
 if __name__ == '__main__':
-    run()
+    run(standalone_mode=False)
